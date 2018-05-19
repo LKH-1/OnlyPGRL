cc4303db87e6d0b907c90065bf5ba3d6fadde673
chagmgang <chagmgang@gmail.com>
2018-05-19 21:52:46 +0900

    need revise

--- true_region_policy_optimization/trpo.py
+++ true_region_policy_optimization/trpo.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import gym
+import numpy as np
 
 def get_copy_var_ops(*, dest_scope_name, src_scope_name):
     op_holder = []
@@ -13,6 +15,15 @@ def get_copy_var_ops(*, dest_scope_name, src_scope_name):
 
     return op_holder
 
+def disconut_rewards(r):
+    discounted_r = np.zeros_like(r, dtype=np.float32)
+    running_add = 0
+    for i in reversed(range(len(r))):
+        running_add = running_add * 0.99 + r[i]
+        discounted_r[i] = running_add
+    discounted_r = (discounted_r - discounted_r.mean())/(discounted_r.std() + 1e-7)
+    return discounted_r
+
 class trpo:
 
     def __init__(self, sess, state_size, action_size):
@@ -21,10 +32,40 @@ class trpo:
         self.sess = sess
 
         self.X = tf.placeholder(tf.float32, [None, self.state_size])
+        self.a = tf.placeholder(tf.float32, [None, self.action_size])
+        self.r = tf.placeholder(tf.float32, [None, 1])
+        self.v_ = tf.placeholder(tf.float32, [None, 1])
 
         self.pi = self._build_anet('pi')
         self.old_pi = self._build_anet('old_pi')
         self.critic = self._build_cnet()
+
+        self.td_error = self.r + 0.99 * self.v_ - self.critic
+        self.closs = tf.square(self.td_error)
+        self.train_cop = tf.train.AdamOptimizer(0.001).minimize(self.closs)
+
+        act_probs = self.pi * self.a
+        act_probs = tf.reduce_sum(act_probs, axis=1)
+        act_probs_old = self.old_pi * self.a
+        act_probs_old = tf.reduce_sum(act_probs_old, axis=1)
+
+        ratios = tf.exp(tf.log(act_probs) - tf.log(act_probs_old))
+        clipped_ratios = tf.clip_by_value(ratios, clip_value_min=1 - 0.2, clip_value_max=1 + 0.2)
+        loss_clip = tf.minimum(tf.multiply(self.td_error, ratios), tf.multiply(self.td_error, clipped_ratios))
+        loss_clip = tf.reduce_mean(loss_clip)
+        entropy = -tf.reduce_sum(self.pi *
+                                     tf.log(tf.clip_by_value(self.pi, 1e-10, 1.0)), axis=1)
+        entropy = tf.reduce_mean(entropy, axis=0)
+        loss = loss_clip + 0.01 * entropy
+        loss = -loss
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.001, epsilon=1e-5)
+        train_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'pi')
+        self.train_aop = optimizer.minimize(loss, var_list=train_vars)
+
+    def learn(self, state, next_state, reward, action):
+        v_ = self.sess.run(self.critic, feed_dict={self.X: next_state})
+        _, _ = self.sess.run([self.train_cop, self.train_aop],
+                                feed_dict={self.X: state, self.v_: v_, self.r: reward, self.a: action})
 
     def _build_anet(self,name):
         with tf.variable_scope(name):
@@ -43,19 +84,47 @@ class trpo:
         copy_op = []
         main_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='pi')
         target_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='old_pi')
-        print('pi', self.sess.run(main_vars))
-        print('old_pi', self.sess.run(target_vars))
         for main_var, target_var in zip(main_vars, target_vars):
             copy_op.append(target_var.assign(main_var.value()))
         
         self.sess.run(copy_op)
-        main_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='pi')
-        target_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='old_pi')
-        print('pi', self.sess.run(main_vars))
-        print('old_pi', self.sess.run(target_vars))
-        
+
+    def choose_action(self, s):
+        a_prob = self.sess.run(self.pi, feed_dict={self.X: [s]})
+        action = np.random.choice(self.action_size, p=a_prob[0])
+        return action    
 
 sess = tf.Session()
-trpo = trpo(sess, 4, 2)
+TRPO = trpo(sess, 4, 2)
 sess.run(tf.global_variables_initializer())
-print(trpo.update_pi_network())
+env = gym.make('CartPole-v0')
+
+for episodes in range(1000):
+    done = False
+    state = env.reset()
+    states = np.empty(shape=[0, 4])
+    actions = np.empty(shape=[0, 2])
+    next_states = np.empty(shape=[0, 4])
+    rewards = np.empty(shape=[0, 1])
+    t = 0
+    while not done:
+        t += 1
+        action_pred = TRPO.choose_action(state)
+        next_state, reward, done, _ = env.step(action_pred)
+        if done:
+            reward = -1
+        else:
+            reward = 0
+        states = np.vstack([states, state])
+        next_states = np.vstack([next_states, next_state])
+        rewards = np.vstack([rewards, reward])
+        action = np.zeros(2)
+        action[action_pred] = 1
+        actions = np.vstack([actions, action])
+        state = next_state
+
+        if done:
+            discounted_rewards = disconut_rewards(rewards)
+            TRPO.learn(states, next_states, discounted_rewards, actions)
+            TRPO.update_pi_network()
+            print(episodes, t)
