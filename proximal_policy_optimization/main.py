76fc22923447b43b12eb7689cac830aabc64ac69
chageumgang <chagmgang@gmail.com>
2018-06-17 15:57:04 +0900

    revise ppo

--- proximal_policy_optimization/main.py
+++ proximal_policy_optimization/main.py
@@ -1,44 +1,38 @@
-from ppo import PPO
-import gym
 import tensorflow as tf
 import numpy as np
-import time
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import gym
+from ppo import PPO
 
 EP_MAX = 1000
 EP_LEN = 200
 GAMMA = 0.9
-A_LR = 0.001
-C_LR = 0.002
+A_LR = 0.0001
+C_LR = 0.0002
 BATCH = 32
 A_UPDATE_STEPS = 10
 C_UPDATE_STEPS = 10
-
+S_DIM, A_DIM = 3, 1
 METHOD = [
     dict(name='kl_pen', kl_target=0.01, lam=0.5),   # KL penalty
     dict(name='clip', epsilon=0.2),                 # Clipped surrogate objective, find this is better
-][1]
+][1]        # choose the method for optimization
+
 
 env = gym.make('Pendulum-v0').unwrapped
-
-cr_net_shape = [3,100,1]
-cr_act_shape = [tf.nn.relu, tf.nn.relu]
-ac_net_shape = [3,100, 100, 1]
-ac_act_shape = [tf.nn.relu, tf.nn.relu]
-act_range = 2
-
-ppo = PPO(act_range, METHOD, A_LR, C_LR)
+ppo = PPO(2, METHOD, A_LR, C_LR, S_DIM, A_DIM, A_UPDATE_STEPS, C_UPDATE_STEPS)
 all_ep_r = []
-saver = tf.train.Saver()
 
 for ep in range(EP_MAX):
     s = env.reset()
     buffer_s, buffer_a, buffer_r = [], [], []
     ep_r = 0
     for t in range(EP_LEN):    # in one episode
-        #if ep % 10 == 0:
-        #    env.render()
-        #    time.sleep(0.01)
-        a = ppo.choose_action(s, act_range)
+        if ep % 10 == 0:
+            env.render()
+        a = ppo.choose_action(s)
         s_, r, done, _ = env.step(a)
         buffer_s.append(s)
         buffer_a.append(a)
@@ -57,9 +51,7 @@ for ep in range(EP_MAX):
 
             bs, ba, br = np.vstack(buffer_s), np.vstack(buffer_a), np.array(discounted_r)[:, np.newaxis]
             buffer_s, buffer_a, buffer_r = [], [], []
-            ppo.update(bs, ba, br, METHOD, A_UPDATE_STEPS, C_UPDATE_STEPS)
-            #saver.save(ppo.sess, './ppo.ckpt')
-
+            ppo.update(bs, ba, br)
     if ep == 0: all_ep_r.append(ep_r)
     else: all_ep_r.append(all_ep_r[-1]*0.9 + ep_r*0.1)
     print(
@@ -67,3 +59,6 @@ for ep in range(EP_MAX):
         "|Ep_r: %i" % ep_r,
         ("|Lam: %.4f" % METHOD['lam']) if METHOD['name'] == 'kl_pen' else '',
     )
+
+plt.plot(np.arange(len(all_ep_r)), all_ep_r)
+plt.xlabel('Episode');plt.ylabel('Moving averaged episode reward');plt.show()
